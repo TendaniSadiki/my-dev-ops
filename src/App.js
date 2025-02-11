e2fa2cab29ced950bd4e625827fc82667bda448b
TENDANISADIKI16@GMAIL.COM <Tendanisadiki16@gmail.com>
2025-02-11 14:49:26 +0200

    {test} 6

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-Hi          
+Hello World from <code>src/App.js</code> file
         </p>
         <a
           className="App-link"
